33861a5c1e4f9535e733085a5da418ebb588cb38
Khaera <sf13371@yandex.ru>
2022-07-10 23:48:04 +0300

    feat: pw13 ready

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62cb02b688222f3bc8a5f961',
+  };
+
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+app.use('/', userRouter);
+app.use('/', cardRouter);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен. Порт: ${PORT}`);
--- models/card.js
+++ models/card.js
@@ -1,30 +1,36 @@
 const mongoose = require('mongoose');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      default: [],
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    link: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        default: [],
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  {
+    versionKey: false,
+    // eslint-disable-next-line comma-dangle
+  }
+);
 
 module.exports = mongoose.model('card', cardSchema);
