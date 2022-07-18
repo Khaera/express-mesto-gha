117f9033bb964cdfde8c54925228345dc0289b73
Khaera <sf13371@yandex.ru>
2022-07-18 19:07:38 +0300

    feat: signin/up routes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
+const login = require('./controllers/login');
+const { createUser } = require('./controllers/users');
+
 const { PORT = 3000 } = process.env;
 
 const { NOT_FOUND } = require('./utils/errors');
@@ -29,6 +32,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use('*', (req, res) => {
   res.status(NOT_FOUND).send({ message: 'Страница не существует.' });
 });
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,12 @@ const userRouter = require('express').Router();
 const {
   getUsers,
   getUser,
-  createUser,
   updateUser,
   updateAvatar,
 } = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
 userRouter.get('/users/:userId', getUser);
-userRouter.post('/users', createUser);
 userRouter.patch('/users/me', updateUser);
 userRouter.patch('/users/me/avatar', updateAvatar);
 
